282f2104875f6ea9f9f670ee225b7ba0a671e080
brunokian <brunokian@hotmail.com>
2022-03-20 06:26:28 -0300

    exercicio 5 e 6, 4.1

--- 1-fundamentos/4-javascript/exercises_4.1.js
+++ 1-fundamentos/4-javascript/exercises_4.1.js
@@ -34,13 +34,52 @@
 
 // exercicio 4
 
-const a = -0.000
+// const a = -0.000
 
-if (a === 0) {
-    console.log ('zero')
-} else if (a > 0) {
-    console.log ('positive')
-} else {
-    console.log ('negative')
+// if (a === 0) {
+//     console.log ('zero')
+// } else if (a > 0) {
+//     console.log ('positive')
+// } else {
+//     console.log ('negative')
+// }
+
+// exercicio 5
+
+// const a = 60
+// const b = 60
+// const c = -61
+
+// if (a <= 0 || b <= 0 || c <= 0 ) {
+//     console.log ('opa, parece que um dos valores recebidos não é valido')
+// } else if (a + b + c === 180) {
+//     console.log (true)
+// } else {
+//     console.log (false)
+// }
+
+// exercicio 6
+
+let chessPiece = 'pawn'
+
+switch (chessPiece) {
+    case 'king': console.log ('one square in any direction')
+        break;
+
+    case 'queen': console.log ('any number of squares in any direction')
+        break;
+
+    case 'rook': console.log ('any number of square in a straight line')
+        break
+
+    case 'bishop': console.log ('any number of square diagonally')
+        break
+
+    case 'knight': console.log ('moves on an extended diagonal from one corner of any two-by-three rectangle of squares to the farthest opposite corner')
+        break
+
+    case 'pawn': console.log ('moves forward exactly one square, or optionally, two squares when on its starting square')
+    
+    default: console.log ('sorry, this is not a chess piece')
+        break;
 }
-
