5c0c7ef2c2d227733af852e99561b19f8e0a8b3f
brunokian <brunokian@hotmail.com>
2022-03-20 05:30:41 -0300

    pasta fundamento, javascript, exercicio 4.1

---
+++ 1-fundamentos/4-javascript/exercises_4.1.js
@@ -0,0 +1,21 @@
+// exercicio 1
+
+// const a = 7
+// const b = 3
+
+// console.log (a+b)
+// console.log (a-b)
+// console.log (a*b)
+// console.log (a/b)
+// console.log (a%b)
+
+// exercicio 2
+
+// const a = 7
+// const b = 8
+
+// if (a > b) {
+//     console.log (a)
+// } else {
+//     console.log (b)
+// }
