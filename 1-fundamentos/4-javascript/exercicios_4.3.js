7affa4bd6677fa894ee863cc4526ea5dafa3d19f
brunokian <brunokian@hotmail.com>
2022-03-20 10:10:57 -0300

    4.3

---
+++ 1-fundamentos/4-javascript/exercicios_4.3.js
@@ -0,0 +1,14 @@
+//exercicio 1
+
+let valores = []
+let dezFatorial = 1
+
+for (i = 1; i < 11; ++i) {
+    valores.push (i)
+}
+
+for (i = 0; i < valores.length; ++i) {
+    dezFatorial * valores[i]
+}
+
+console.log (dezFatorial)
