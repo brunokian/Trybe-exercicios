1c038ec495ef269dab5fe25c24367f224c2c411c
brunokian <brunokian@hotmail.com>
2022-03-20 05:47:00 -0300

    exercicios 3 e 4 do 4.1

--- 1-fundamentos/4-javascript/exercises_4.1.js
+++ 1-fundamentos/4-javascript/exercises_4.1.js
@@ -19,3 +19,28 @@
 // } else {
 //     console.log (b)
 // }
+
+// exercicio 3
+
+// const a = 11
+// const b = 11
+// const c = 12
+
+// if (a > b && a > c) {
+//     console.log (a)
+// } else if (b > a && b > c) {
+//     console.log (b)
+// } else (console.log (c))
+
+// exercicio 4
+
+const a = -0.000
+
+if (a === 0) {
+    console.log ('zero')
+} else if (a > 0) {
+    console.log ('positive')
+} else {
+    console.log ('negative')
+}
+
