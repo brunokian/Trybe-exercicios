eb6161f7d78ea6f760dcb2a7495de90a623dbd7c
brunokian <brunokian@hotmail.com>
2022-03-20 21:47:24 -0300

    exercicio 2,3 4.3

--- 1-fundamentos/4-javascript/exercicios_4.3.js
+++ 1-fundamentos/4-javascript/exercicios_4.3.js
@@ -1,3 +1,5 @@
+// falta o exercicio 4
+
 //exercicio 1
 
 // let valores = []
@@ -25,14 +27,62 @@
 
 //exercicio 2
 
-let texto = 'tryber'
+// let texto = 'tryber'
 
-let inverso = texto.split ('').reverse().join("")
+// let inverso = texto.split ('').reverse().join("")
 
-console.log (inverso)
+// console.log (inverso)
 
 // .split (''): cria array, separando a string em letras (inclusive os espaços)
 // .split (' '): cria array, separando a string em palavras
 // .split (' ', 3): neste caso, cria-se a array parando no terceiro valor
 // .reverse(): inverte a ordem dos elementos do array
-// .join (): retorna o array como string
+// .join (): retorna o array como string (entre aspas é possivel colocar algo entre os valores)
+
+//outra forma
+
+// let texto = 'tryber'
+// let inverso = ''
+
+// for (i = 0; i < texto.length; ++i) {
+//     inverso += texto [texto.length -1 -i]
+// }
+
+// console.log (inverso)
+
+//exercicio 3
+
+// let array = ['java', 'javascript', 'python', 'html', 'css'];
+
+// let maior = array [0]
+// let menor = array [0]
+
+// for (i = 0; i < array.length; ++i) {
+//     if (maior.length < array[i].length) {
+//         maior = array[i]
+//     }
+// }
+
+// for (i = 0; i < array.length; ++i) {
+//     if (menor.length > array[i].length) {
+//         menor = array[i]
+//     }
+// }
+
+// console.log(maior);
+// console.log(menor)
+
+//exercicio 4
+
+let numerosPrimos = []
+let primoMaior = 0
+
+for (i = 1; i < 51; ++i) {
+    numerosPrimos.push (i)
+} for (u = 2; u < numerosPrimos[i]; ++u) {
+    if (numerosPrimos )
+}
+
+
+
+console.log (numerosPrimos)
